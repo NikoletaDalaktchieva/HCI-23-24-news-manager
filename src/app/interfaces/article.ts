8a906a8a4f553434394c779e33495a1b9cce17f0
andro <kroxitrock@gmail.com>
2023-10-08 16:48:23 +0200

    Mock news service

--- src/app/interfaces/article.ts
+++ src/app/interfaces/article.ts
@@ -2,14 +2,14 @@ import { Category } from '../enums/category';
 
 export interface Article {
   id: number;
-  tittle: string;
+  title: string;
   subtitle?: string;
   category: Category;
   abstract: string;
   body?: string;
   update_date?: Date;
-  // image_data;
-  // image_media_type;
-  // thumbnail_data;
-  // thumbnail_media_type;
+  image_data?: string;
+  image_media_type?: string;
+  thumbnail_data?: string;
+  thumbnail_media_type?: string;
 }
