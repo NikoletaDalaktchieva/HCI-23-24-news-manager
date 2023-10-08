5f34b2a34f26efde41007423b0ea7c012f1ccff4
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2023-10-08 17:46:19 +0200

    nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,9 +20,10 @@ import { MatOptionModule } from '@angular/material/core';
 
 import { FormsModule } from '@angular/forms';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent, ArticleListComponent],
+  declarations: [AppComponent, LoginFormComponent, ArticleListComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
