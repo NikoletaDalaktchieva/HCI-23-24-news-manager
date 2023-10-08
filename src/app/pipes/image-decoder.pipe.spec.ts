aa4a793ecf7072d730af37fe708a5537607cda95
andro <kroxitrock@gmail.com>
2023-10-08 17:22:02 +0200

    Add decoding pipe

---
+++ src/app/pipes/image-decoder.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ImageDecoderPipe } from './image-decoder.pipe';
+
+describe('ImageDecoderPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ImageDecoderPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
