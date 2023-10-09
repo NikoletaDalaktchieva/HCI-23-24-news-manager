aa02d9c19f69dee6283bf707f0021d74119b61cb
Andrey Strinsk <kroxitrock@gmail.com>
2023-10-09 18:17:30 +0200

    Fix login

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from '../interfaces/user';
 import { LoginService } from '../services/login.service';
 import { Router } from '@angular/router';
+import { NewsService } from '../services/news.service';
 
 @Component({
   selector: 'app-login-form',
@@ -12,13 +13,20 @@ export class LoginFormComponent {
   public username = '';
   public password = '';
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(
+    private loginService: LoginService,
+    private newsService: NewsService,
+    private router: Router
+  ) {}
 
   onSubmit() {
     console.log('Submitted');
 
     this.loginService.login(this.username, this.password).subscribe({
-      next: () => this.router.navigate(['/']),
+      next: (user) => {
+        this.newsService.setUserApiKey(user.apikey);
+        this.router.navigate(['/']);
+      },
       error: () => {
         console.log('An error occurred while logging in!');
       },
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -4,14 +4,14 @@ import { Observable } from 'rxjs';
 import { envConfig } from 'src/app/environment/config';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Category } from '../enums/category';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NewsService {
-  private newsUrl = 'http://sanger.dia.fi.upm.es/pui-rest-news/articles'; // URL to web api
-  private articleUrl = 'http://sanger.dia.fi.upm.es/pui-rest-news/article'; // URL to web api
+  private rootUrl = 'http://sanger.dia.fi.upm.es/pui-rest-news';
+  private articleListUrl = `${this.rootUrl}/articles`;
+  private articleUrl = `${this.rootUrl}/article`;
 
   private apiKey?: string;
 
@@ -24,8 +24,7 @@ export class NewsService {
     }),
   };
 
-  // Modifies the APIKEY with the received value
-  setUserApiKey(apiKey: string) {
+  public setUserApiKey(apiKey: string) {
     this.apiKey = apiKey;
     this.httpOptions = {
       headers: new HttpHeaders({
@@ -35,23 +34,12 @@ export class NewsService {
     };
   }
 
-  setAnonymousApiKey() {
+  public setAnonymousApiKey() {
     this.setUserApiKey(envConfig.apiKey);
   }
 
-  // Returns the list of news contain elements with the following fields:
-  // {"id":...,
-  //  "id_user":...,
-  //  "abstract":...,
-  //  "subtitle":...,
-  //  "update_date":...,
-  //  "category":...,
-  //  "title":...,
-  //  "thumbnail_image":...,
-  //  "thumbnail_media_type":...}
-
   getArticles(): Observable<Article[]> {
-    return this.http.get<Article[]>(this.newsUrl, this.httpOptions);
+    return this.http.get<Article[]>(this.articleListUrl, this.httpOptions);
   }
 
   deleteArticle(article: Article | number): Observable<Article> {
@@ -59,17 +47,6 @@ export class NewsService {
     const url = `${this.articleUrl}/${id}`;
     return this.http.delete<Article>(url, this.httpOptions);
   }
-
-  // Returns an article which contains the following elements:
-  // {"id":...,
-  //  "id_user":...,
-  //  "abstract":...,
-  //  "subtitle":...,
-  //  "update_date":...,
-  //  "category":...,
-  //  "title":...,
-  //  "image_data":...,
-  //  "image_media_type":...}
 
   getArticle(id: number): Observable<Article> {
     console.log('Requesting article id=' + id);
